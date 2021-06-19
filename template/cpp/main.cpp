6c719b7d4b77e2ea3e37b35de6925a6626f32024
66ed3gs <66ed3gs@gmail.com>
2021-06-19 19:10:06 +0900

    add(template): add c++ template

---
+++ template/cpp/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+#define REP(i, n) for (long i = 0; i < n; i++)
+#define REPR(i, n) for (long i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for (long i = m; i < n; i++)
+
+#define ALL(v) v.begin(), v.end()
+
+typedef long long ll;
+
+// sort(ALL(a)), sortALL(a), greater<>())
+//
+
+using namespace std;
+
+int main() {
+    REP(i, 10) cout << i << endl;
+    REPR(l, 10) cout << l << endl;
+    FOR(i, 10, 20) cout << i << endl;
+}
+
