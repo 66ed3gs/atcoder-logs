67b3479919317bb0532c1eee713ffb0eca874c0a
66ed3gs <66ed3gs@gmail.com>
2021-06-22 11:54:51 +0900

    change(template): change c++ template

--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -8,15 +8,18 @@
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; i >= 0; i--)
 #define FOR(i, m, n) for (int i = m; i < n; i++)
-
 #define ALL(v) v.begin(), v.end()
 
 typedef long long ll;
 
-// sort(ALL(a)), sortALL(a), greater<>())
-//
+// sort(ALL(a))
+// sortALL(a), greater<>())
+// int max = *max_element(ALL(a));
+// int min = *min_element(ALL(a));
 
 using namespace std;
 
-int main() { int n; }
-
+int main() {
+    int n;
+    cin >> n;
+}
