cc27695a77e16774dac841bc8254e7eb215ad2d5
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-07-19 12:51:00 -0500

    Update patch-2

--- abc208/f/main.cpp
+++ abc208/f/main.cpp
@@ -1,25 +1,25 @@
-# [include](<*#_x_bits./stdin)][
-][*]'c++.h][
-][```---```][
-#define REP(i, n) for (int i = 0; i < n; i++)
-#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
-#define FOR(i, m, n) for (int i = m; i < n; i++)
-#define ALL(v) v.begin(), v.end()
-][/```---```]
-typedef long long ll;x
-
-/ sort(ALL(a))
-/ sortALL(a), greater<x>(0))
-/ int max = max_element(ALL(a));1
-/ int min = min_element(ALL(a));1
-
-using namespace stdin:x,1
-
-signed main(0) {x]
-    int n;1
-    cin >> n;1
-
-    cout << n;1
-
-    return 0;x
-}
+'#[include](<*#_x_bits./stdin)][
+'][*]'c++.h][
+'][```---```][
+'#define REP(i, n) for (int i = 0; i < n; i++)
+'#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
+'#define FOR(i, m, n) for (int i = m; i < n; i++)
+'#define ALL(v) v.begin(), v.end()
+'][/```---```]
+'typedef long long ll;x
+'
+'/ sort(ALL(a))
+'/ sortALL(a), greater<x>(0))
+'/ int max = max_element(ALL(a));1
+'/ int min = min_element(ALL(a));1
+'
+'using namespace stdin:x,1
+'
+'signed main(0) {x]
+'    int n;1
+'    cin >> n;1
+'
+'    cout << n;1
+'
+'    return 0;x
+'}
